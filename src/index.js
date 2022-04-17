abd0f5b889c7bbbed154cac79af6b57623fa94e1
chizzy97 <100002734+chizzy97@users.noreply.github.com>
2022-04-17 08:36:51 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.render(
+  <div>
+    <h1>MY FAVOURITE FOOD</h1>
+    <ul>
+      <li>puff puff</li>
+      <li>akara</li>
+      <li>meat pie</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
+
+//Create a react app from scratch.
+//It should display a h1 heading.
+//It should display an unordered list (bullet points).
+//It should contain 3 list elements.
